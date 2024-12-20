0098da3deb9c2988cf3f19d8116a02f6b9c71835
Amama-TB <amama-tul.bushra@ada.ac.uk>
2024-12-20 15:15:25 +0000

    Flask Basic Blog Walkthrough

--- models.py
+++ models.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 
 db = SQLAlchemy()
+
 
 class BlogPost(db.Model):
     id = db.mapped_column(db.Integer, primary_key=True)
@@ -13,9 +15,9 @@ class BlogPost(db.Model):
 
     def __str__(self):
         return f'"{self.title}" by {self.author} ({self.created_at:%Y-%m-%d})'
-    
+
     @staticmethod
     def get_post_lengths():
         # An example of how to use raw SQL inside a model
         sql = text("SELECT length(title) + length(content) FROM blog_post")
-        return db.session.execute(sql).scalars().all() # Returns just the integers
+        return db.session.execute(sql).scalars().all()  # Returns just the integers
